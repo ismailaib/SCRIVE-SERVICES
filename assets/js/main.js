47b5de23548fa5b67ca6bc5d37854c46aee4d5bc
ismailaib <ismailaitbouhmad@gmail.com>
2022-09-29 22:41:12 +0100

    add code

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+        var x = document.getElementById("learn-more");
+        if (x.style.display === "none") {
+          x.style.display = "inline";
+        } else {
+          x.style.display = "none";
+        }
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+        var x = document.getElementById("learn-more");
+        if (x.style.display === "none") {
+          x.style.display = "inline";
+        } else {
+          x.style.display = "none";
+        }
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav-link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('nav')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+
+/*=============== CALCULATE JS ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+
+
