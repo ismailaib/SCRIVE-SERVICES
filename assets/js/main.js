9b490ed821de839747b2cc1294851f7b1dec9bb2
ismailaib <ismailaitbouhmad@gmail.com>
2022-10-06 17:01:20 +0100

    add code

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,7 @@ const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
+
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
