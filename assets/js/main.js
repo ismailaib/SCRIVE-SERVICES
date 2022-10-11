3b3e049c39c27769bc9cdf4b626a1936e5afcb24
ismailaib <ismailaitbouhmad@gmail.com>
2022-10-11 15:17:04 +0100

    add contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,13 @@ $(document).ready(function() {
     });
 });
 
+
+$('.cardb').on('click', function() {
+    var value = $(this).attr('name');
+    $('#subject').val(value);
+})
+
+
 $(document).ready(function() {
     $(window).scroll( function(){
         $('.choose-crd').each( function(i){
@@ -101,3 +108,16 @@ $(document).ready(function() {
         }); 
     });
 });
+/*=============== scroll show bridge ===============*/
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.inputs').each( function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},600);
+            }
+        }); 
+    });
+});
+
